385324dfce91ae4950997a6675a050f75249e5a3
dvarre <varre.dimitri@gmail.com>
2025-06-12 12:07:54 +0200

    feat(#5): Implement room creation interface

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import "./init"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { WebSocketProvider } from './components/WebSocketProvider.tsx'
+import { RouterProvider } from "react-router-dom"
+import router from "./routes/router.tsx"
+import './tailwind.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <WebSocketProvider>
-      <App />
+        <RouterProvider router={router}/>
     </WebSocketProvider>
   </StrictMode>,
 )
---
+++ src/routes/router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import LobbyCreation from "../pages/LobbyCreation";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <LobbyCreation/>
+    }
+
+    // TODO Créer une page et redirection pour les 404 et autres erreurs
+]);
+
+
+export default router;
